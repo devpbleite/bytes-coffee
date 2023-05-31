b72e3591e875d665df69182bf5a445f8a44aa864
Pablo Leite <pabloleite03@gmail.com>
2023-05-31 09:36:00 -0300

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.content}>
-        <h1>Bubble Coffee</h1>
+        <h1>Bytes Coffee</h1>
         <div className={styles.line} />
         <p className='dark-bg'>
           Se você busca um momento de tranquilidade em meio à agitação do dia a
